8419e85ed4f95a1fad34f3488d30e06c2be13842
Jakob Fix <jakob@jfix.com>
2018-01-25 23:12:29 +0100

    Clean up and reformat

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ const T = new Twit({
 })
 const getFollowers = require('twitter-followers')
 
-// const op = 'jfix'
-// const oe = 'cmarchand7276'
 const oe = 'OECD'
 const op = 'OECD_pubs'
 
@@ -23,52 +21,50 @@ let commonFollowerIds = []
 let commonFollowers = []
 
 // first, get OECD_pubs followers
-getFollowers(T, op)
-    .then((followers) => {
-        opFollowers = followers
-        console.log(`Got ${opFollowers.length} followers for @${op}.`)
+getFollowers(T, op).then((followers) => {
+  opFollowers = followers
+  console.log(`Got ${opFollowers.length} followers for @${op}.`)
 
-        // second, get OECD's followers
-        getFollowers(T, oe)
-        .then((followers) => {
-            oeFollowers = followers
-            console.log(`Got ${oeFollowers.length} followers for @${oe}.`)
+  // second, get OECD's followers
+  getFollowers(T, oe).then((followers) => {
+    oeFollowers = followers
+    console.log(`Got ${oeFollowers.length} followers for @${oe}.`)
 
-            // extract common followers
-            commonFollowerIds = _.intersection(opFollowers, oeFollowers)
+    // extract common followers
+    commonFollowerIds = _.intersection(opFollowers, oeFollowers)
 
-            // some basic infos:
-            console.log(`# followers @${oe}: ${oeFollowers.length}.`)
-            console.log(`# followers @${op}: ${opFollowers.length}.`)
-            console.log(`# of common followers: ${commonFollowerIds.length}.`)
+    // some basic infos:
+    console.log(`# followers @${oe}: ${oeFollowers.length}.`)
+    console.log(`# followers @${op}: ${opFollowers.length}.`)
+    console.log(`# of common followers: ${commonFollowerIds.length}.`)
 
-            // more info (per follower) to be collected below
+    // more info (per follower) to be collected below
 
-            // take the array of commonFollowers and chunk it into smaller arrays of max 100
-            // because the users/lookup api endpoint accepts max 100 users in one go
-            const batchFollowerIds = _.chunk(commonFollowerIds, 100)
-            _.forEach(batchFollowerIds, (oneHundred) => {
-                const params = { user_id: oneHundred.join(','), include_entities: false }
-                T.post('users/lookup', params, function(err, users, response) {
-                    if (err) throw err
-                    _.forEach(users, (user) => {
-                        // have a look here to see what other fields could be extracted:
-                        // https://developer.twitter.com/en/docs/accounts-and-users/follow-search-get-users/api-reference/get-users-lookup
-                        commonFollowers.push({
-                            name: user.name,
-                            id_str: user.id_str,
-                            followers_count: user.followers_count,
-                            screen_name: user.screen_name,
-                            description: user.description,
-                            url: user.url
-                        })
-                    })
-                    // console.log(commonFollowers)
-                    fs.writeFile(`${oe}-${op}-common-followers.json`, JSON.stringify(commonFollowers), (err) => {
-                      if (err) throw err
-                      console.log('Result file successfully saved.')
-                    })
-                })
-            })
+    // take the array of commonFollowers and chunk it into smaller arrays of max 100
+    // because the users/lookup api endpoint accepts max 100 users in one go
+    const batchFollowerIds = _.chunk(commonFollowerIds, 100)
+    _.forEach(batchFollowerIds, (oneHundred) => {
+      const params = { user_id: oneHundred.join(','), include_entities: false }
+      T.post('users/lookup', params, function (err, users, response) {
+        if (err) throw err
+        _.forEach(users, (user) => {
+          // have a look here to see what other fields could be extracted:
+          // https://developer.twitter.com/en/docs/accounts-and-users/follow-search-get-users/api-reference/get-users-lookup
+          commonFollowers.push({
+            name: user.name,
+            id_str: user.id_str,
+            followers_count: user.followers_count,
+            screen_name: user.screen_name,
+            description: user.description,
+            url: user.url
+          })
         })
+              // console.log(commonFollowers)
+        fs.writeFile(`${oe}-${op}-common-followers.json`, JSON.stringify(commonFollowers), (err) => {
+          if (err) throw err
+          console.log('Result file successfully saved.')
+        })
+      })
     })
+  })
+})
